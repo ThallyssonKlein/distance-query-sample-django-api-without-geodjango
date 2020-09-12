c6eb0c08888352ceaa61d58f31aaf90e32fadabb
thallyssonklein <thallyssonklein@gmail.com>
2020-09-12 07:57:05 -0300

    Django Secret no longer exposed

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This code is intended for use in development, not production. It was not made to
 ## Why?
 
 I faced difficulties to query distances with django using sqlite and without geodjango.
+
+**Ps:** For reasons of Github security policies, I cannot keep the django secret exposed. For this code to work create a .env at the root of the directory and set DJANGO_SECRET=[your secret]
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7z4rytw%d_-n0^4qy3%y%3-_)yy^4lg=8_^s970xw#^x-2_u6n'
+SECRET_KEY = os.environ.get('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
